57b1825530407977b2ba29b536efd0a316c73550
Mike R <mickeau@gmail.com>
2025-04-22 15:16:30 +0100

    Removed instruction comments

--- tasktracker.py
+++ tasktracker.py
@@ -1,29 +1,4 @@
 #Task Tracker
-'''Add, Update, and Delete tasks
-Mark a task as in progress or done
-List all tasks
-List all tasks that are done
-List all tasks that are not done
-List all tasks that are in progress
-Here are some constraints to guide the implementation:
-
-You can use any programming language to build this project.
-Use positional arguments in command line to accept user inputs.
-Use a JSON file to store the tasks in the current directory.
-The JSON file should be created if it does not exist.
-Use the native file system module of your programming language to interact with the JSON file.
-Do not use any external libraries or frameworks to build this project.
-Ensure to handle errors and edge cases gracefully.
-
-Task Properties
-Each task should have the following properties:
-
-id: A unique identifier for the task
-description: A short description of the task
-status: The status of the task (todo, in-progress, done)
-createdAt: The date and time when the task was created
-updatedAt: The date and time when the task was last updated
-Make sure to add these properties to the JSON file when adding a new task and update them when updating a task.'''
 
 import datetime
 import json
@@ -138,7 +113,7 @@ def list():
     print("\nList of all tasks:\n")
     for task in taskList:
         if task["id"] > 0:
-            print(task["id"], ". ", task["description"])
+            print(task["id"], ":", task["description"])
     print("\n")
 
 def listDone():
@@ -146,21 +121,21 @@ def listDone():
     for task in taskList:
         if task["id"] > 0:
             if task["status"] == "done":
-                print(task["description"])
+                print(task["id"], ":", task["description"])
 
 def listInProgress():
     print("\nList of tasks in progress:\n")
     for task in taskList:
         if task["id"] > 0:
             if task["status"] == "in progress":
-                print(task["id"], ". ", task["description"])
+                print(task["id"], ":", task["description"])
 
 def listToDo():
     print("\nList of tasks yet to be done:\n")
     for task in taskList:
         if task["id"] > 0:
             if task["status"] == "todo":
-                print(task["id"], ". ", task["description"])
+                print(task["id"], ":", task["description"])
 
 if __name__ == "__main__":
     try:
